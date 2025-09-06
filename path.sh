5bf2520673a1a5c191883d49c541a993cd48b089
cantabile-kwok <58417810+cantabile-kwok@users.noreply.github.com>
2025-09-06 22:53:02 +0100

    typo in path.sh

--- path.sh
+++ path.sh
@@ -1,4 +1,5 @@
 #!/bin/bash                                                                                                                                                                           
 
 export PATH=$PWD/utils:$PWD/lscodec/bin:$PATH
-export PYTHONPATH=$PWD:$PYTHONPATH  # to add `paralle_wavegan` into pythonpath
+export PYTHONPATH=$PWD:$PYTHONPATH  # to add `lscodec` into pythonpath
+
